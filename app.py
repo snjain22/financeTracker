60062adcb915f1b4b556f254689351cd2e9422a3
Sambhav Nath Jain <118072637+snjain22@users.noreply.github.com>
2025-11-04 15:15:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import gspread
-from oauth2client.service_account import Credentials
+from google.oauth2.service_account import Credentials
 import pandas as pd
 import plotly.express as px
 import os
